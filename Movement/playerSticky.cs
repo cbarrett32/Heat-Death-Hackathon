1d7f976cd7ec15d853bcc027e628e36bd9671878
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2018-10-21 18:09:57 -0400

    Add files via upload

---
+++ Movement/playerSticky.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSticky : MonoBehaviour
+{
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		col.gameObject.transform.parent = gameObject.transform;
+
+	}
+	void OnCollisionExit2D(Collision2D col)
+	{
+		col.gameObject.transform.parent = null;
+	}
+}
