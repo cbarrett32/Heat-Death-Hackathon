0d0cb32f6ff58a693f0042d1d85f73f4eb070a7e
Charlie Barrett <37375131+cbarrett32@users.noreply.github.com>
2018-10-21 18:06:19 -0400

    Add files via upload

---
+++ DamageableEnemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DamageableEnemy : MonoBehaviour, IDamagable
+{
+	public int health = 5;
+	//private AudioSource asteroidDestroyAudio;
+	bool hasPlayed = false;
+
+	public void Update()
+	{
+//		if (hasPlayed == false) {
+//			Debug.Log ("Playing audio " + hasPlayed);
+//			AudioSource explosionAudio = gameObject.GetComponent<AudioSource> ();
+//			Debug.Log (explosionAudio.clip.name);
+//			explosionAudio.Play ();
+//			//AudioSource.PlayClipAtPoint (explosionAudio.clip, transform.position);
+//			hasPlayed = true;
+//		}
+	}
+
+	public void TakeDamage(int damageTaken)
+	{
+		health -= damageTaken;
+
+		if (health <= 0)
+		{
+			Debug.Log("I have been defeated!!!!");
+
+			Debug.Log ("Playing Audio");
+			AudioSource explosionAudio = gameObject.GetComponent<AudioSource> ();
+
+			//Debug.Log (explosionAudio);
+
+			if (explosionAudio == null) {
+				Debug.Log ("Can't play due to missing audioSource");
+			}
+
+			//explosionAudio.PlayOneShot (explosionAudio.clip);
+			explosionAudio.Play ();
+
+			AudioSource.PlayClipAtPoint (explosionAudio.clip, transform.position);
+			//gameObject.SetActive (false);
+			gameObject.transform.localScale = Vector3.zero;
+
+			Destroy( gameObject, 2.0f ); 
+		}
+	}
+}
